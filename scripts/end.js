2187ba4212175744b1c0c836028726a6a4211cea
Sunischit(Sun) Thapa <60706330+Solaris50@users.noreply.github.com>
2021-09-06 18:01:10 -0400

    Updated code includes better UI and question selection

--- scripts/end.js
+++ scripts/end.js
@@ -1,21 +1,34 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const endValue = localStorage.getItem('mostRecentScore')
+const showErrorMessage = document.querySelector('.errorMessage')
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [] 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText = mostRecentScore
-
-username.addEventListener('keyup', () => {
-  saveScoreBtn.disabled = !username.value
-})
+let mostRecentScore = "";
+  
+  if (endValue <= 400) {
+    mostRecentScore = "The Italian Game"
+  } else if (endValue <= 800) {
+    mostRecentScore = "The Sicilian Defense"
+  } else if (endValue < 1600) {
+    mostRecentScore = "The French Defense"
+  } else {
+    mostRecentScore = "The Ruy-Lopez"
+  }
+finalScore.innerText = mostRecentScore;
 
 saveHighScore = e => {
   e.preventDefault()
-
+  if(!username.value == true){
+    showErrorMessage.style.display = "inline";
+  }
+  else{
+    saveScoreBtn.disabled = false;  
+    showErrorMessage.style.display = "none";
   const score = {
     score: mostRecentScore,
     name: username.value
@@ -23,12 +36,13 @@ saveHighScore = e => {
 
   highScores.push(score)
 
-  highScores.sort((a,b) => {
+  highScores.sort((a, b) => {
     return b.score - a.score
   })
 
-  highScores.splice(5)
+  highScores.splice(10)
 
   localStorage.setItem('highScores', JSON.stringify(highScores))
   window.location.assign('/')
+}
 }
--- scripts/index.js
+++ scripts/index.js
@@ -8,57 +8,55 @@ const progressBarFull = document.querySelector("#progressBarFull");
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
+let value = 0
 let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
   {
-    question: 'What is your favourite fruit',
-    choice1: 'apple',
-    choice2: 'banna',
-    choice3: 'peach',
-    choice4: 'orange',
-    answer: 1,
+    question: 'What colour do you want to start with?',
+    choice1: 'black',
+    choice2: 'white',
+    choice3: "Doesn't matter",
+    choice4: 'orange'
   },
   {
-    question: 'What is your favourite vegetable',
-    choice1: 'carrot',
-    choice2: 'cucumber',
-    choice3: 'onion',
-    choice4: 'lettuce',
-    answer: 2,
+    question: 'What play style best describes you?',
+    choice1: 'I like to play the slow game and gradually over take my opponent with positional play',
+    choice2: 'I like to trap my opponents and take a quick win through big win early',
+    choice3: 'I like to go all out attack and get board space as fast as possible',
+    choice4: "I like a balanced approach with a good mix of creating offensive opportunities as well as having a good fundamental structure "
   },
   {
-    question: 'What is your favourite animal',
-    choice1: 'fish',
-    choice2: 'cheetah',
-    choice3: 'lion',
-    choice4: 'dog',
-    answer: 3,
+    question: 'What is your current rating?',
+    choice1: '< 1000',
+    choice2: '1000 < rating > 1400',
+    choice3: '1400 < rating > 2000',
+    choice4: '> 2000'
   },
   {
-    question: 'What is your favourite planet',
-    choice1: 'mars',
-    choice2: 'earth',
-    choice3: 'moon',
-    choice4: 'Sun #donthate',
-    answer: 4,
+    question: 'How much complexity and theory will you want in your opening?',
+    choice1: 'Very little theory, I just want a simple opening',
+    choice2: 'Moderate theory, I want some complexity based on opponent responses',
+    choice3: 'Heavy theory, I want to study many if not all the possibilities or advanced positions',
+    choice4: 'No theory'
   }
 ]
 
 const SCORE_POINTS = 100;
 const MAX_QUESTIONS = 4;
+let questionIndex = 0;
 
 startGame = () => {
   questionCounter = 0;
-  score = 0;
+  value = 0;
   availableQuestions = [...questions]
   getNewQuestion()
 }
 
 getNewQuestion = () => {
   if (availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
-    localStorage.setItem('mostRecentScore', score)
+    localStorage.setItem('mostRecentScore', value)
 
     return window.location.assign('../pages/end.html')
   }
@@ -66,8 +64,6 @@ getNewQuestion = () => {
   questionCounter++
   progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
   progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)*100}%`
-
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length)
 
   currentQuestion = availableQuestions[questionIndex]
   question.innerText = currentQuestion.question;
@@ -80,6 +76,7 @@ getNewQuestion = () => {
   availableQuestions.splice(questionIndex, 1)
 
   acceptingAnswers = true;
+
 }
 
 choices.forEach(choices => {
@@ -90,16 +87,20 @@ choices.forEach(choices => {
     const selectedChoice = e.target
     const selectedAnswer = selectedChoice.dataset['number']
 
-    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-    if (classToApply === 'correct') {
-      incrementScore(SCORE_POINTS)
+    if(selectedAnswer == 1) {
+      value += 100;
+    } else if( selectedAnswer == 2){
+      value += 200;
+    } else if (selectedAnswer == 3){
+      value += 300;
+    } else {
+      value += 400;
     }
 
-    selectedChoice.parentElement.classList.add(classToApply);
+    selectedChoice.parentElement.classList.add('correct');
 
     setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToApply)
+      selectedChoice.parentElement.classList.remove('correct')
       getNewQuestion()
     }, 1000)
   })
